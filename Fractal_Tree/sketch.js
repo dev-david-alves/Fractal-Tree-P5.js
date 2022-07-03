94cf83f84664ab42c381044a1153d1e7d8a605d1
dev-david-alves <davidalves9090@gmail.com>
2022-07-03 12:26:41 -0300

    Fractal Tree Finished.

--- Fractal_Tree/sketch.js
+++ Fractal_Tree/sketch.js
@@ -1,18 +1,26 @@
-let len = 120;
+// Definitions
+let len = 130;
 let angle = 0;
 let slider;
 
 function setup() {
-  createCanvas(500, 500);
+  createCanvas(450, 450);
+  
+  // Create a slider
   slider = createSlider(0, PI / 2, PI / 4, 0.01);
-  slider.position(10, 10);
+  slider.style('width', '220px');
+  slider.position(windowWidth / 2 - 110, 15);
 }
 
 function branch(len) {
+  // Draw the branch
   line(0, 0, 0, -len);
   
+  // To prevent inifinite recursion
   if(len > 4) {
+    // Move to the end of the branch
     translate(0, -len);
+    // Rotate the branch by the angle and then call the function again
     push();
     rotate(angle * 0.5);
     branch(len * 0.7);
@@ -26,9 +34,14 @@ function branch(len) {
 
 function draw() {
   background(0);
+
+  // Get the value of the slider
   angle = slider.value();
   translate(width / 2, height);
+
   stroke(255);
   strokeWeight(2);
+
+  // Call the function the first time
   branch(len);
 }
